d1c18fd63fa5503d8d7a0a4bbd36c5c3ed4193a0
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-03-23 16:04:43 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.01 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.java
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] solution(String[] strlist) {
         int[] answer = new int[strlist.length];
-        // int[] answer = {}; 일때는 에러, 초기화 하니까 됐다.
+        // int[] answer = {}; 일때는 에러, 이건 길이가 0인 배열을 생성한거라 값을 넣으려면 길이를 정해줘야함.
             for(int i=0; i<strlist.length; i++){
                 answer[i] = strlist[i].length();       
             } 

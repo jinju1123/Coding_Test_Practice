bced6b2631570c8ecfe7cabeeed0ebbce849e96d
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-03-25 23:26:35 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.33 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array) {
+        Arrays.sort(array);
+        int answer = 0;
+        return answer = array[array.length/2]; 
+    }
+}

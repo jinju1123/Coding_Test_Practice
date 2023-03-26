21ea8f8b5b8600c4e19af5fc23e46d0fa4588515
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-03-26 23:08:11 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.07 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, String letter) {
+        my_string = my_string.replaceAll(letter, "");
+        return my_string;
+    }
+}

38e4ed3293b7605230d90d6e1d1b58393258eaaf
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-03-27 16:45:16 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -13,3 +13,13 @@ class Solution {
         return (int)answer;
     }
 }
+    
+    // 위에서 부터 쓰면 조건식을 두개씩 안써도 되는구나 생각해보니!!
+    // 초간단하게 풀수 있다. price를 굳이 double로 바꾸지 않아도 됨.
+    //    int answer = 0;
+    //
+    //    if(price>=500000) return (int)(price*0.8);
+    //    if(price>=300000) return (int)(price*0.9);
+    //    if(price>=100000) return (int)(price*0.95);
+    //
+    //    return price;

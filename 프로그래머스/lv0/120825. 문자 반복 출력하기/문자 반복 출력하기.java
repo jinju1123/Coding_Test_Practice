ec300b174ce5c8e238e82080deb25b0acfecc040
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-03-28 16:47:30 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.20 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -9,4 +9,13 @@ class Solution {
         String answer = String.join("", arr);
         return answer;
     }
+    
+    // 선언과 동시에 값 넣기, 문자열 끼리는 합칠때 더하기로 할 수 있잖아 진주야.. join 괜히씀
+    // String answer = "";
+    // String[] arr = my_string.split("");
+    // for(int i=0; i<arr.length; i++) {
+    //  answer += arr[i].repeat(n);
+    // }
+    // return answer;
+
 }

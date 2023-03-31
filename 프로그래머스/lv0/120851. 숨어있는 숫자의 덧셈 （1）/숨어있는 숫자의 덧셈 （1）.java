a2a1a3623cf6816632d27e5e214c3cbadb5cb174
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-03-31 15:25:36 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.19 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -1,13 +1,21 @@
 import java.util.*;
 
 class Solution {
-    public int solution(String my_string) {
+     public int solution(String my_string) {
+    //     int answer = 0;
+    //     my_string = my_string.replaceAll("[^0-9]","");
+    //     char[] str = my_string.toCharArray();
+    //     for(int i=0; i<str.length; i++){
+    //         answer += str[i]-'0';
+    //     }
+    //     return answer;
+    // }
         int answer = 0;
-        my_string = my_string.replaceAll("[^0-9]","");
-        char[] str = my_string.toCharArray();
-        for(int i=0; i<str.length; i++){
-            answer += str[i]-'0';
+        String str = my_string.replaceAll("[^0-9]", "");
+        for( char ch : str.toCharArray() ) {
+            answer += Character.getNumericValue(ch);
         }
         return answer;
     }
 }
+

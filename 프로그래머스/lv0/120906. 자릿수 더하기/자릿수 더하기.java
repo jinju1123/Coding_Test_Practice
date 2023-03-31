4dc0b1298081d147693eda43cb794fcbe0604b0b
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-03-31 16:02:14 +0900

    [level 0] Title: 자릿수 더하기, Time: 3.29 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -9,4 +9,16 @@ class Solution {
         }
         return answer;
     }
-}
+// 이거 이해가 안가는데 ....?
+// class Solution {
+//     public int solution(int n) {
+//         int answer = 0;
+
+//         while(n>0){
+//             answer+=n%10;
+//             n/=10;
+//         }
+
+//         return answer;
+//     }
+ }

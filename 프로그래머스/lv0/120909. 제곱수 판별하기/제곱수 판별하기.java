e6552115a6476f38106746825918954486dcb35a
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-04-01 14:19:46 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.04 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,16 +1,19 @@
-import java.util.*;
+import java.lang.Math;
 class Solution {
     public int solution(int n) {
-        List<Integer> divi = new ArrayList<>();
-        for(int i=1; i<=n; i++){
-            if(n%i == 0){
-                divi.add(i);
-            }
-        }
+//         List<Integer> divi = new ArrayList<>();
+//         for(int i=1; i<=n; i++){
+//             if(n%i == 0){
+//                 divi.add(i);
+//             }
+//         }
         
-        if(divi.size() % 2 == 0){
-            return 2;
-        }
-        return 1 ;
-    }
+//         if(divi.size() % 2 == 0){
+//             return 2;
+//         }
+//         return 1 ;
+//     }
+// 제곱근 구하는 함수 쓰면 간단하다  
+        return n % Math.sqrt(n) == 0 ? 1 : 2;
+}
 }

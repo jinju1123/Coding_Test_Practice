4685531a770c5e33188a29125334399af9bba427
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-04-02 20:05:56 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -1,17 +1,25 @@
 class Solution {
     public int solution(int hp) {
-        int answer = 0;
-        int x = hp / 5;
-        int y = 0;
-        int z = 0;
-        if(hp % 5 >= 3){
-            y = (hp % 5) / 3;
-            z = (hp % 5) % 3;
-        } else {
-            y = hp % 5;
-            z = 0;
-        }
-        answer = x + y + z;
+        // int answer = 0;
+        // int x = hp / 5;
+        // int y = 0;
+        // int z = 0;
+        // if(hp % 5 >= 3){
+        //     y = (hp % 5) / 3;
+        //     z = (hp % 5) % 3;
+        // } else {
+        //     y = hp % 5;
+        //     z = 0;
+        // }
+        // answer = x + y + z;
+        // return answer;
+        
+        // 간단하게 가능
+        int answer = hp / 5;   // answer에 5로 나눈 몫 넣기
+        hp %= 5;  // hp를 5로 나눈 나머지 값으로 변경
+        answer += hp / 3;  // 기존 answer에 3로 나눈 몫 넣기
+        hp %= 3;
+        answer += hp / 1;
         return answer;
     }
 }

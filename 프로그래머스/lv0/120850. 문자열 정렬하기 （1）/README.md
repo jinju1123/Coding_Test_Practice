7ec6b4fcaeb2d15981620bd6a75475f00d8b93cf
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-04-03 23:34:13 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.46 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/README.md
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 정렬하기 (1) - 120850 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120850) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120850?language=java) 
 
 ### 성능 요약
 
-메모리: 73.8 MB, 시간: 0.56 ms
+메모리: 70.6 MB, 시간: 0.46 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -2,10 +2,17 @@ import java.util.*;
 class Solution {
     public int[] solution(String my_string) {
         my_string = my_string.replaceAll("[a-z]","");
-        String[] str = my_string.split("");
-        int[] answer = new int[str.length];
-        for(int i=0; i<str.length; i++){
-            answer[i] = Integer.parseInt(str[i]);
+        // String[] str = my_string.split("");
+        // int[] answer = new int[str.length];
+        // for(int i=0; i<str.length; i++){
+        //    answer[i] = Integer.parseInt(str[i]);
+        //}
+        // Arrays.sort(answer);
+        
+        // 이렇게 charAt()(string 값 하나씩 가져옴) 사용해도 가능 
+        int[] answer = new int[my_string.length()];
+        for(int i=0; i<my_string.length(); i++){
+            answer[i] = my_string.charAt(i) - '0'; // '0'(아스키코드 48)을 빼면 자동형변환되면서 1~됨
         }
         Arrays.sort(answer);
         return answer;

aa4fb1cfb18ee6bc030f84c349c2b8e387771e8c
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-04-04 16:50:59 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/README.md
+++ 프로그래머스/lv0/120910. 세균 증식/README.md
@@ -1,10 +1,10 @@
 # [level 0] 세균 증식 - 120910 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120910) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120910?language=java) 
 
 ### 성능 요약
 
-메모리: 74.1 MB, 시간: 0.03 ms
+메모리: 71.2 MB, 시간: 0.03 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -4,5 +4,11 @@ class Solution {
         int answer = 0;
         double num = Math.pow(2,t);
         return answer = n * (int)num;
+        // 어렵게 생각하지 않아도 된다.
+        // for(int i=0; i<t; i++) {
+        //    n *= 2
+        // }
+        // return n;
+        // i가 식 안에 없어도 된다. 돌기만 하면 되니까... 이걸 몰랐네ㅡㅡ
     }
 }

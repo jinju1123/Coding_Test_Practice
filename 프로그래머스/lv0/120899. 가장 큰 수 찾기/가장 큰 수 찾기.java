3f2feb01f2a5c7f55d1f63460792f074d138df62
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-04-06 17:59:51 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -3,21 +3,31 @@ import java.util.List;
 
 class Solution {
     public int[] solution(int[] array) {
-        List<Integer> list = new ArrayList<>();
-        int max = array[0];
-        int a = 0;
-        for(int i=0; i<array.length; i++){
-            if(array[i] > max) {
-                max = array[i];
-                a = i;
-            }
-        }
-        list.add(max);
-        list.add(a);
+//         List<Integer> list = new ArrayList<>();
+//         int max = array[0];
+//         int a = 0;
+//         for(int i=0; i<array.length; i++){
+//             if(array[i] > max) {
+//                 max = array[i];
+//                 a = i;
+//             }
+//         }
+//         list.add(max);
+//         list.add(a);
         
-        int[] answer = new int[list.size()];
-        for(int i=0; i<list.size(); i++){
-            answer[i] = list.get(i);
+//         int[] answer = new int[list.size()];
+//         for(int i=0; i<list.size(); i++){
+//             answer[i] = list.get(i);
+//         }
+//         return answer;
+        
+        // 너무 간단하게 풀 수 있다.. 어차피 결과는 무조건 원소 2개의 배열
+        int[] answer = new int[2];
+        for(int i=0; i<array.length; i++){
+            if(array[i] > answer[0]){
+                answer[0] = array[i];
+                answer[1] = i;
+            }
         }
         return answer;
     }

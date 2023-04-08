6592ced8ec0770cc7dfb0fc803feb6307d536cc8
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-04-08 22:40:54 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i=1; i<=1000; i++){
+            if((n*i)%6==0){
+                answer = (n*i)/6;
+                break;
+            }
+        }
+
+        
+        return answer;
+    }
+}

5a4fc7056e6f2a5543805fafb860b27d8b867072
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-04-09 15:41:42 +0900

    [level 0] Title: 숫자 찾기, Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,13 +1,21 @@
 class Solution {
     public int solution(int num, int k) {
-        int answer = -1;
-        String numstr = ""+num;
-        for(int i=0; i<numstr.length(); i++){
-            if((int)numstr.charAt(i) - '0' == k){
-                answer = i+1;
-                break;
-            }
-        }
-        return answer;
+        // int answer = -1;
+        // String numstr = ""+num;
+        // for(int i=0; i<numstr.length(); i++){
+        //     if((int)numstr.charAt(i) - '0' == k){
+        //         answer = i+1;
+        //         break;
+        //     }
+        // }
+        // return answer;
+        // indexOf를 이용한 풀이
+        String numStr = String.valueOf(num);
+        String kStr = String.valueOf(k);
+        
+        int answer = numStr.indexOf(kStr);
+        
+        return answer == -1 ? -1 : answer+1;
+        
     }
 }

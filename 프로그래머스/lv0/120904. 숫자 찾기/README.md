957c32d75254168e4047e0524f1d5bdf883f75dd
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-04-09 15:35:47 +0900

    [level 0] Title: 숫자 찾기, Time: 7.61 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/README.md
+++ 프로그래머스/lv0/120904. 숫자 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 숫자 찾기 - 120904 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120904) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120904?language=java) 
 
 ### 성능 요약
 
-메모리: 83 MB, 시간: 7.60 ms
+메모리: 77.6 MB, 시간: 7.61 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,12 +1,12 @@
 class Solution {
     public int solution(int num, int k) {
-        int answer = 0;
+        int answer = -1;
         String numstr = ""+num;
         for(int i=0; i<numstr.length(); i++){
-            if((int)numstr.charAt(i) - 48 == k){
+            if((int)numstr.charAt(i) - '0' == k){
                 answer = i+1;
                 break;
-            } else answer = -1;
+            }
         }
         return answer;
     }

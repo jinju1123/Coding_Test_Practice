8be0c4e8cff3546b6c42a5d2ee351462c6268f9b
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-04-12 18:48:15 +0900

    [level 0] Title: 가까운 수, Time: 0.26 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/README.md
+++ 프로그래머스/lv0/120890. 가까운 수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 가까운 수 - 120890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120890) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120890?language=java) 
 
 ### 성능 요약
 
-메모리: 77.4 MB, 시간: 0.25 ms
+메모리: 78.3 MB, 시간: 0.26 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
@@ -3,6 +3,9 @@ import java.util.Collections;
 
 class Solution {
     public int solution(int[] array, int n) {
+        // n의 제일 큰수가 100이라서 min을 100으로 설정
+        // Math.abs는 절대값 표시! 음수, 양수 상관 X
+        // 제일 작은 값을 min에 넣기
         int min = 100;
         for(int i=0; i<array.length; i++){
             if(Math.abs(array[i]-n) < min){
@@ -10,14 +13,29 @@ class Solution {
             }
         }
         
+        // 새로운 배열을 담을 List 생성하고 min이 나온 array[i]를 담기
         ArrayList<Integer> arr = new ArrayList<>();
         for(int i=0; i<array.length; i++){
             if(Math.abs(array[i]-n) == min){
                 arr.add(array[i]);
             }
         }
+        
+        // 오름차순으로 정리해서 제일 작은 수를 0번으로 오게 배치
         Collections.sort(arr);
         
         return arr.get(0);
+        
+        
+        // 간단하게 풀기
+        // int answer = 0;
+        // Arrays.sort(array);
+        // for(int i = 1 ; i < array.length ; i++){
+        //     if(Math.abs(n-array[0]) > Math.abs(n-array[i])){
+        //         array[0] = array[i];
+        //     }
+        // }
+        // answer = array[0];
+        // return answer;
     }
 }

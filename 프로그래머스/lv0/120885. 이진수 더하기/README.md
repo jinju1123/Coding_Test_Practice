fd4d0e1038373eba4fc122c8053b642b2d548d3e
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-04-16 16:20:00 +0900

    [level 0] Title: 이진수 더하기, Time: 0.03 ms, Memory: 65.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120885. 이진수 더하기/README.md
+++ 프로그래머스/lv0/120885. 이진수 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 이진수 더하기 - 120885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120885) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120885?language=java) 
 
 ### 성능 요약
 
-메모리: 73.9 MB, 시간: 0.03 ms
+메모리: 65.3 MB, 시간: 0.03 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.java
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.java
@@ -1,9 +1,11 @@
 class Solution {
     public String solution(String bin1, String bin2) {
-        String answer = "";
-        int num1 = Integer.parseInt(bin1, 2);
-        int num2 = Integer.parseInt(bin2, 2);
-        int sum = num1 + num2;
-        return answer = Integer.toBinaryString(sum);
+        // String answer = "";
+        // int num1 = Integer.parseInt(bin1, 2);
+        // int num2 = Integer.parseInt(bin2, 2);
+        // int sum = num1 + num2;
+        // return answer = Integer.toBinaryString(sum);
+        // 간단하게 줄이기
+        return Integer.toBinaryString(Integer.parseInt(bin1, 2)+Integer.parseInt(bin2, 2));
     }
 }

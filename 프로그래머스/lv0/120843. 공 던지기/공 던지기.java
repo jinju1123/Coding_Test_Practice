75abea49175f41e90d043ebfd8b1155e24b93ad8
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-04-18 15:39:39 +0900

    [level 0] Title: 공 던지기, Time: 0.02 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] numbers, int k) {
+        int answer = 0;
+        int idx = 0;
+        for(int i=1; i<k; i++){
+            idx += 2;
+        }
+        idx %= numbers.length;
+        return answer = numbers[idx];
+    }
+}

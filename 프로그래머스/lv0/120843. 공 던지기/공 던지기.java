b1fde843050c503b771d918c65852397a0cd6c13
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-04-18 15:47:28 +0900

    [level 0] Title: 공 던지기, Time: 0.02 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
@@ -1,11 +1,8 @@
 class Solution {
     public int solution(int[] numbers, int k) {
-        int answer = 0;
-        int idx = 0;
-        for(int i=1; i<k; i++){
-            idx += 2;
-        }
-        idx %= numbers.length;
-        return answer = numbers[idx];
+        // 인덱스는 2씩 증가됨
+        // 첫번째로 공을 던지는 사람은 인덱스가 0이므로 (k-1)
+        // 배열의 길이보다 커질 수 있으므로 배열의 길이로 나눈 나머지를 대입
+        return numbers[(k-1)*2 % numbers.length]; 
     }
 }

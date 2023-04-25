b494d736acd308f953a57571263b7dd76f8b852f
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-04-25 13:56:29 +0900

    [unrated] Title: 배열의 원소 삭제하기, Time: 2.09 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181844. 배열의 원소 삭제하기/README.md
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 배열의 원소 삭제하기 - 181844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181844?language=java) 
 
 ### 성능 요약
 
-메모리: 74.3 MB, 시간: 1.95 ms
+메모리: 84.5 MB, 시간: 2.09 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
@@ -2,20 +2,31 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr, int[] delete_list) {
-        // arr배열과 같은 list 생성
+//         // arr배열과 같은 list 생성
+//         List<Integer> list = new ArrayList<>();
+//         for(int i : arr){
+//             list.add(i);
+//         }
+        
+//         // 중복값 찾기
+//         for(int i=0; i<arr.length; i++){
+//             for(int j=0; j<delete_list.length; j++){
+//                 if(arr[i]==delete_list[j]){
+//                     list.remove(Integer.valueOf(arr[i]));
+//                 }
+//             }
+//         }
+//         return list.stream().mapToInt(i->i).toArray();
+        
+        // 더 간단하게 풀기
         List<Integer> list = new ArrayList<>();
         for(int i : arr){
             list.add(i);
         }
-        
-        // 중복값 찾기
-        for(int i=0; i<arr.length; i++){
-            for(int j=0; j<delete_list.length; j++){
-                if(arr[i]==delete_list[j]){
-                    list.remove(Integer.valueOf(arr[i]));
-                }
-            }
+        for(int d : delete_list){
+            list.remove(Integer.valueOf(d));
         }
+       
         return list.stream().mapToInt(i->i).toArray();
     }
 }

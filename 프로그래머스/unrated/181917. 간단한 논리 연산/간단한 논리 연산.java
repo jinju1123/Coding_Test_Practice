1418fb84d276fabc4f445361eee811a88cd158e2
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-05-02 11:38:26 +0900

    [unrated] Title: 간단한 논리 연산, Time: 0.03 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.java
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -1,17 +1,18 @@
 class Solution {
     public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
-        boolean answer = false;
-        boolean a = false;
-        boolean b = false;
-        if(x1 || x2) {
-            a = true;
-        } 
-        if(x3 || x4) {
-            b = true;
-        }
-        if(a && b){
-            answer = true;
-        }
-        return answer;
+        // boolean answer = false;
+        // boolean a = false;
+        // boolean b = false;
+        // if(x1 || x2) {
+        //     a = true;
+        // } 
+        // if(x3 || x4) {
+        //     b = true;
+        // }
+        // if(a && b){
+        //     answer = true;
+        // }
+        // 간단하게 풀기 가능
+        return (x1||x2)&&(x3||x4);
     }
 }

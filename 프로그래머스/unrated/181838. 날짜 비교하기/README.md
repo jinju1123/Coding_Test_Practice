91158692c56ab0cae32247ca97d8a3c6bd329627
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-05-07 14:16:07 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.03 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/README.md
+++ 프로그래머스/unrated/181838. 날짜 비교하기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 날짜 비교하기 - 181838 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181838) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181838?language=java) 
 
 ### 성능 요약
 
-메모리: 74 MB, 시간: 0.03 ms
+메모리: 76.8 MB, 시간: 0.03 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,16 +1,30 @@
 class Solution {
     public int solution(int[] date1, int[] date2) {
-        if(date1[0]<date2[0]){
-            return 1;
-        } else if (date1[0]==date2[0]){
-            if(date1[1]<date2[1]){
-                return 1;
-            } else if(date1[1]==date2[1]) {
-                if(date1[2]<date2[2]){
-                    return 1;
-                }
+        // if(date1[0]<date2[0]){
+        //     return 1;
+        // } else if (date1[0]==date2[0]){
+        //     if(date1[1]<date2[1]){
+        //         return 1;
+        //     } else if(date1[1]==date2[1]) {
+        //         if(date1[2]<date2[2]){
+        //             return 1;
+        //         }
+        //     }
+        // }
+        // return 0;
+        
+        // 나처럼 복잡하게 푼사람 또 나밖에 없네.. ㅎㅎ
+        // case1. for문으로 풀기
+        int answer=0;
+        for(int i=0; i<date1.length; i++){
+            if(date1[i]<date2[i]){
+                answer=1;
+                break;
+            } else if(date1[i]>date2[i]){
+                answer=0;
+                break;
             }
         }
-        return 0;
+        return answer;
     }
 }

533bd3e68d6fd224a34083b7afac03a9aafc7e14
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-05-07 14:21:49 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.66 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 class Solution {
     public int solution(int[] date1, int[] date2) {
         // if(date1[0]<date2[0]){
@@ -15,16 +17,23 @@ class Solution {
         
         // 나처럼 복잡하게 푼사람 또 나밖에 없네.. ㅎㅎ
         // case1. for문으로 풀기
-        int answer=0;
-        for(int i=0; i<date1.length; i++){
-            if(date1[i]<date2[i]){
-                answer=1;
-                break;
-            } else if(date1[i]>date2[i]){
-                answer=0;
-                break;
-            }
-        }
-        return answer;
+        // int answer=0;
+        // for(int i=0; i<date1.length; i++){
+        //     if(date1[i]<date2[i]){
+        //         answer=1;
+        //         break;
+        //     } else if(date1[i]>date2[i]){
+        //         answer=0;
+        //         break;
+        //     }
+        // }
+        // return answer;
+        
+        // case2. LocalDate 이용하기
+        
+        LocalDate dateA = LocalDate.of(date1[0], date1[1], date1[2]);
+        LocalDate dateB = LocalDate.of(date2[0], date2[1], date2[2]);
+        
+        return dateA.isBefore(dateB) ? 1 : 0;
     }
 }

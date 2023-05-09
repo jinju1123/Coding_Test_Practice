d8043e7c7319ed1e17319f7246f5c5b28dbb779a
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-05-09 17:56:08 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.01 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
@@ -1,16 +1,28 @@
 class Solution {
     public int[] solution(String my_string) {
+        // int[] answer = new int[52];
+        // char[] alph = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        //                  'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        //                 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        //                  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        // for(int i=0; i<my_string.length(); i++){
+        //     for(int j=0; j<alph.length; j++){
+        //         if(my_string.charAt(i) == alph[j]){
+        //             answer[j]++;
+        //         }
+        //     }
+        // }
+        // return answer;
+        
+        // char 이용해서 간단하게 풀기. 무식하게 풀지 말구
+        
         int[] answer = new int[52];
-        char[] alph = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                         'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-                        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                         'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-        for(int i=0; i<my_string.length(); i++){
-            for(int j=0; j<alph.length; j++){
-                if(my_string.charAt(i) == alph[j]){
-                    answer[j]++;
-                }
-            }
+        for(int i = 0; i < my_string.length(); i++){
+            char c = my_string.charAt(i);
+            if(c >= 'a')
+                answer[c - 'a' + 26]++;
+            else
+                answer[c - 'A']++;
         }
         return answer;
     }

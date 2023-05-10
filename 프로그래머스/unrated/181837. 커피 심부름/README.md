1d9cc6e1f14f771b9be697d5a63ac0abd8627cff
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-05-10 22:52:11 +0900

    [unrated] Title: 커피 심부름, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181837. 커피 심부름/README.md
+++ 프로그래머스/unrated/181837. 커피 심부름/README.md
@@ -1,10 +1,10 @@
 # [unrated] 커피 심부름 - 181837 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181837) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181837?language=java) 
 
 ### 성능 요약
 
-메모리: 76.6 MB, 시간: 0.04 ms
+메모리: 76.6 MB, 시간: 0.02 ms
 
 ### 구분
 
--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
@@ -2,12 +2,22 @@ class Solution {
     public int solution(String[] order) {
         int answer = 0;
         
+        // for(String s : order){
+        //     if(s.contains("americano")){
+        //         answer += 4500;
+        //     } else if(s.contains("cafelatte")){
+        //         answer += 5000;
+        //     } else if(s.contains("anything")){
+        //         answer += 4500;
+        //     }
+        // }
+        
+        // 초 간단 방법이 있었다.. 좀만 더 생각해볼걸
+        
         for(String s : order){
-            if(s.contains("americano")){
-                answer += 4500;
-            } else if(s.contains("cafelatte")){
+            if(s.contains("cafelatte")){
                 answer += 5000;
-            } else if(s.contains("anything")){
+            } else {
                 answer += 4500;
             }
         }

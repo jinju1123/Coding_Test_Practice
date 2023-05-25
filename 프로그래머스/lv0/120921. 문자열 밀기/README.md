012074fd6f8ef13a2536b63cda9539b152773d7f
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-05-25 19:04:20 +0900

    [level 0] Title: 문자열 밀기, Time: 2.01 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/README.md
+++ 프로그래머스/lv0/120921. 문자열 밀기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 밀기 - 120921 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120921) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120921?language=java) 
 
 ### 성능 요약
 
-메모리: 74.5 MB, 시간: 1.28 ms
+메모리: 75.9 MB, 시간: 2.01 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,18 +1,21 @@
 class Solution {
     public int solution(String A, String B) {
-        int answer = 0;
-        String str = A;
+//         int answer = 0;
+//         String str = A;
         
-        for(int i=0; i<A.length(); i++){
-            if(str.equals(B)) {
-                return answer;
-            }
+//         for(int i=0; i<A.length(); i++){
+//             if(str.equals(B)) {
+//                 return answer;
+//             }
             
-            String lastA = str.substring(str.length()-1);   // 마지막 글자만 뽑기
-            str = lastA + str.substring(0, str.length()-1); // 마지막 글자 + 앞 4글자
-            answer++;
-        }
+//             String lastA = str.substring(str.length()-1);   // 마지막 글자만 뽑기
+//             str = lastA + str.substring(0, str.length()-1); // 마지막 글자 + 앞 4글자
+//             answer++;
+//         }
         
-        return -1;
+//         return -1;
+        
+        // 엄청난 코드 발견
+        return (B+B).indexOf(A);
     }
 }

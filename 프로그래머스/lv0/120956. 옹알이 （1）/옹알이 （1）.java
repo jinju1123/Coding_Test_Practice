2ed228eb701e27c8dd330311c8e9f9943d1556e3
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-05-27 18:57:47 +0900

    [level 0] Title: 옹알이 (1), Time: 1.80 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        //aya 4
+        //ayaye ayawoo ayama 12
+        //ayayewoo ayawooye ayayema ayamaye ayawooma ayamawoo 24
+        //ayayewooma ayayemawoo ayawooyema ayawoomaye ayamayewoo ayamawooye 24
+            
+        String[] strArr = {"aya","ye","woo","ma"};
+        
+        List<String> list = new ArrayList<>();
+        
+        for(int i=0; i<strArr.length; i++){
+            for(int j=0; j<strArr.length; j++)
+                if(i!=j) {
+                    list.add(strArr[i]+strArr[j]);
+                }
+        }
+        
+        String[] strArr2 = list.toArray(new String[0]);
+        List<String> list2 = new ArrayList<>();
+        
+        for(int i=0; i<strArr2.length; i++){
+            for(int j=0; j<strArr.length; j++) {
+                if(!strArr2[i].contains(strArr[j])){
+                    list2.add(strArr2[i]+strArr[j]);
+                }
+            }
+        }
+        
+        String[] strArr3 = list2.toArray(new String[0]);
+        List<String> list3 = new ArrayList<>();
+        
+        for(int i=0; i<strArr3.length; i++){
+            for(int j=0; j<strArr.length; j++) {
+                if(!strArr3[i].contains(strArr[j])){
+                    list3.add(strArr3[i]+strArr[j]);
+                }
+            }
+        }
+        
+        list.addAll(list2);
+        list.addAll(list3);
+        list.add("aya"); list.add("ye"); list.add("woo"); list.add("ma"); 
+        
+        for(int i=0; i<list.size(); i++){
+            for(int j=0; j<babbling.length; j++){
+                if(list.get(i).equals(babbling[j])){
+                    answer++;
+                }
+            }
+        }
+        
+        System.out.println(list.size());
+        
+        return answer;
+    }
+}

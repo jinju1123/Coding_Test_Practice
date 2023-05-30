d8cec8c7c2882053ecaa5cdbae23fecfea4b7eb4
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-05-30 17:12:14 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,17 +1,16 @@
 class Solution {
     public int solution(int[] numbers) {
-        
-        int sumAll = 0;
+
         int sum = 0;
         
         for(int i=0; i<=9; i++){
-            sumAll += i;
+            sum += i;
         }
         
         for(int n : numbers){
-            sum += n;
+            sum -= n;
         }
         
-        return sumAll-sum;
+        return sum;
     }
 }

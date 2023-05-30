1bce2d5898a1163b72f39b2da87c610e5208fe17
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-05-30 16:45:29 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/README.md
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 핸드폰 번호 가리기 - 12948 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12948) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12948?language=java) 
 
 ### 성능 요약
 
-메모리: 75.3 MB, 시간: 1.52 ms
+메모리: 73.5 MB, 시간: 0.03 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -2,15 +2,23 @@ class Solution {
     public String solution(String phone_number) {
         
         // 풀이 1
-        String back = phone_number.substring(phone_number.length()-4);
-        String answer = "*".repeat(phone_number.length()-4);
-        return answer+back;
+        // String back = phone_number.substring(phone_number.length()-4);
+        // String answer = "*".repeat(phone_number.length()-4);
+        // return answer+back;
         
         // 풀이 2
         // String front = phone_number.substring(0, phone_number.length()-4);
         // String back = phone_number.substring(phone_number.length()-4);
         // String answer = front.replaceAll("[0-9]","*") + back;
         // return answer;
+        
+        //풀이 3
+        char[] ch = phone_number.toCharArray();
+        for(int i = 0; i < ch.length - 4; i ++){
+            ch[i] = '*';
+        }
+        return String.valueOf(ch);
+        
         
     }
 }

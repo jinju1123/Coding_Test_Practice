6305a26f1095812c1f760c547798e138f3b57bb0
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-05-30 16:55:31 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        for(int i=0; i<signs.length; i++){
+            if(!signs[i]){
+                absolutes[i] *= -1;
+            }
+        }
+        
+        for(int i : absolutes) {
+            answer += i;
+        }
+        
+        return answer;
+    }
+}

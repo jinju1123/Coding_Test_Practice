ab678a3763d1e39b43bc008e5971bbf36d50f024
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-06-02 23:52:34 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.35 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/README.md
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 내림차순으로 배치하기 - 12917 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12917) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12917?language=java) 
 
 ### 성능 요약
 
-메모리: 74.9 MB, 시간: 0.37 ms
+메모리: 73.6 MB, 시간: 0.35 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
@@ -2,11 +2,22 @@ import java.util.*;
 class Solution {
     public String solution(String s) {
         
-        String[] strArr = s.split("");
+//         String[] strArr = s.split("");
+//         Arrays.sort(strArr, Collections.reverseOrder());
+//         return String.join("", strArr);
         
-        Arrays.sort(strArr, Collections.reverseOrder());
+        // StringBuilder와 친해지기
+        
+        char[] charArr = s.toCharArray();
+        Arrays.sort(charArr);
+        
+        // String.valueOf는 매개변수 값을 String 생성자 함수에 전달 후 새로운 문자열 객체를 반환함
+        // String.valueOf는 char[]을 String으로 반환해준다. 배열 중에선 문자 배열만 가능한듯
+        StringBuilder sb = new StringBuilder(String.valueOf(charArr));
+        sb.reverse();
+        
+        return sb.toString();
         
         
-        return String.join("", strArr);
     }
 }

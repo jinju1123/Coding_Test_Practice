4aa54462cec9ad9f29fae278275722c675d0f3d1
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-06-10 13:21:52 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,25 +1,37 @@
 class Solution {
     public int solution(int n) {
         
-        int answer = 0;
+//         String str = Integer.toBinaryString(n);
         
-        String str = Integer.toBinaryString(n);
+//         int nlen = 0;
+//         for(char c : str.toCharArray()){
+//             if(c=='1') nlen++;
+//         }
         
-        int nlen = 0;
-        for(char c : str.toCharArray()){
-            if(c=='1') nlen++;
-        }
+//         while(true) {
+//             n++;
+//             String num = Integer.toBinaryString(n);
+//             int numlen = 0;
+//             for(char c : num.toCharArray()){
+//                 if(c=='1') numlen++;
+//             }
+//             if(nlen==numlen) break;
+//         }
         
-        while(true) {
+//         return n;
+        
+        // bitCount()란 메서드가 있었다!! 새로 배웠당
+        // bitCount() - 이진법으로 변환 후 1의 개수를 반환
+        
+        int nlen = Integer.bitCount(n);
+        
+        while(true){
             n++;
-            String num = Integer.toBinaryString(n);
-            int numlen = 0;
-            for(char c : num.toCharArray()){
-                if(c=='1') numlen++;
-            }
-            if(nlen==numlen) break;
+            int nnlen = Integer.bitCount(n);
+            if(nlen == nnlen) break;
         }
         
         return n;
+        
     }
 }

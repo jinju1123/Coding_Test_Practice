b3033e33cddad7cba47d088e6cea2892673bdf43
jinju1123 <118646964+jinju1123@users.noreply.github.com>
2023-06-11 20:42:20 +0900

    [level 1] Title: 최소직사각형, Time: 2.20 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/README.md
+++ 프로그래머스/lv1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=java) 
 
 ### 성능 요약
 
-메모리: 73.5 MB, 시간: 2.09 ms
+메모리: 83.1 MB, 시간: 2.20 ms
 
 ### 구분
 
--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -1,22 +1,33 @@
 class Solution {
     public int solution(int[][] sizes) {
-        int maxW = 0;
-        int maxH = 0;
+//         int maxW = 0;
+//         int maxH = 0;
         
-        for(int i=0; i<sizes.length; i++){
-           int ww = Math.max(sizes[i][0],sizes[i][1]);
-            if(ww > maxW) {
-                maxW = ww;
-            }
+//         for(int i=0; i<sizes.length; i++){
+//            int ww = Math.max(sizes[i][0],sizes[i][1]);
+//             if(ww > maxW) {
+//                 maxW = ww;
+//             }
+//         }
+        
+//         for(int i=0; i<sizes.length; i++){
+//             int hh = Math.min(sizes[i][0],sizes[i][1]);
+//             if(hh > maxH) {
+//                 maxH = hh;
+//             }
+//         }
+        
+//         return maxW * maxH;
+        
+//         // 간단하게!
+        
+        int width = 0, height = 0;
+        
+        for (int[] size : sizes) {
+            width = Math.max(width, Math.max(size[0], size[1]));
+            height = Math.max(height, Math.min(size[0], size[1]));
         }
         
-        for(int i=0; i<sizes.length; i++){
-            int hh = Math.min(sizes[i][0],sizes[i][1]);
-            if(hh > maxH) {
-                maxH = hh;
-            }
-        }
-        
-        return maxW * maxH;
+        return width * height;
     }
 }

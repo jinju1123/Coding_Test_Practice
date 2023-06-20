ed59a932a5c8108c9bf5ced196c9699cf78e8668
Leejinju <118646964+jinju1123@users.noreply.github.com>
2023-06-20 19:09:18 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.java
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -3,28 +3,24 @@ import java.time.*;
 class Solution {
     public String solution(int a, int b) {
         
+        // String answer = "";
+        
+        // LocalDate date = LocalDate.of(2016, a, b);
+        // return date.getDayOfWeek().toString().substring(0,3);
+        
+        // 내장 메서드 안쓰는 풀이!! 깔끔하당
+        
         String answer = "";
-        
-        LocalDate date = LocalDate.of(2016, a, b);
-        DayOfWeek day = date.getDayOfWeek();
-        int dayNumber = day.getValue();
-        
-        if(dayNumber==1){
-            answer = "MON";
-        } else if(dayNumber==2){
-            answer = "TUE";
-        } else if(dayNumber==3){
-            answer = "WED";
-        } else if(dayNumber==4){
-            answer = "THU";
-        } else if(dayNumber==5){
-            answer = "FRI";
-        } else if(dayNumber==6){
-            answer = "SAT";
-        } else if(dayNumber==7){
-            answer = "SUN";
+        String[] day = { "FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU" };
+        int[] date = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        int allDate = 0;
+        for (int i = 0; i < a - 1; i++) {
+            allDate += date[i];
         }
-        
+        allDate += (b - 1);
+        answer = day[allDate % 7];
+
         return answer;
+        
     }
 }
--- 프로그래머스/lv1/12901. 2016년/README.md
+++ 프로그래머스/lv1/12901. 2016년/README.md
@@ -1,10 +1,10 @@
 # [level 1] 2016년 - 12901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901?language=java) 
 
 ### 성능 요약
 
-메모리: 87.6 MB, 시간: 1.42 ms
+메모리: 75.8 MB, 시간: 0.03 ms
 
 ### 구분
 

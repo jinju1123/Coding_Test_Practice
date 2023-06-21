9cb95874b236c72bd170a4df7643a844fcbb1dfb
Leejinju <118646964+jinju1123@users.noreply.github.com>
2023-06-21 17:48:27 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.38 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/README.md
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/README.md
@@ -1,10 +1,10 @@
 # [unrated] 명예의 전당 (1) - 138477 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138477) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138477?language=java) 
 
 ### 성능 요약
 
-메모리: 71.5 MB, 시간: 0.28 ms
+메모리: 75 MB, 시간: 0.38 ms
 
 ### 구분
 
--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -3,36 +3,57 @@ import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
         
+//         int[] answer = new int[score.length];
+        
+//         ArrayList<Integer> list = new ArrayList<>();
+        
+//         if(k < score.length) {
+//             for(int i=0; i<k; i++){
+//                 list.add(score[i]);
+//                 Collections.sort(list);
+//                 answer[i] = list.get(0);
+//             }
+
+//             for(int i=k; i<score.length; i++){
+//                 if(score[i]>list.get(0)){
+//                     list.remove(0);
+//                     list.add(score[i]);
+//                     Collections.sort(list);
+//                     answer[i] = list.get(0);
+//                 } else {
+//                     answer[i] = list.get(0);
+//                 }
+//             }
+//         } else {
+//             for(int i=0; i<score.length; i++){
+//                 list.add(score[i]);
+//                 Collections.sort(list);
+//                 answer[i] = list.get(0);
+//             }
+//         }
+
+//         return answer;
+        
+        // 우선순위 큐로 풀기
         int[] answer = new int[score.length];
         
-        ArrayList<Integer> list = new ArrayList<>();
+        // 기본 오름차순으로 우선순위 정렬 
+        // (1, 10, 5) -> (1, 5, 10)으로 가장 낮은 숫자인 1이 우선순위가 제일 높음
+        // 반대로 하고 싶으면 
+        // PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-        if(k < score.length) {
-            for(int i=0; i<k; i++){
-                list.add(score[i]);
-                Collections.sort(list);
-                answer[i] = list.get(0);
+        for(int i=0; i<score.length; i++){
+            
+            pq.add(score[i]);
+            if(pq.size() > k) {
+                pq.remove();  // 우선순위가 가장 높은 값(첫번째) 삭제
             }
-
-            for(int i=k; i<score.length; i++){
-                if(score[i]>list.get(0)){
-                    list.remove(0);
-                    list.add(score[i]);
-                    Collections.sort(list);
-                    answer[i] = list.get(0);
-                } else {
-                    answer[i] = list.get(0);
-                }
-            }
-        } else {
-            for(int i=0; i<score.length; i++){
-                list.add(score[i]);
-                Collections.sort(list);
-                answer[i] = list.get(0);
-            }
+            
+            answer[i] = pq.peek(); // 우선순위가 가장 높은 값(첫번째) 가져오기
         }
-
         
         return answer;
+        
     }
 }

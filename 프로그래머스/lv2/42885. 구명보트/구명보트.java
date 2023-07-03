e022e20d1f8d4b0efa905d9e383b22506a201825
Leejinju <118646964+jinju1123@users.noreply.github.com>
2023-07-03 15:22:27 +0900

    [level 2] Title: 구명보트, Time: 12.93 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.java
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -3,23 +3,34 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         
-        int answer = 0;
+//         int answer = 0;
+        
+//         Arrays.sort(people);
+        
+//         int p1 = 0;
+//         int p2 = people.length-1;
+        
+//         while(p2 >= p1){
+//             int w = people[p2];
+//             p2--;
+//             if(w + people[p1] <= limit){
+//                 p1++;
+//             }
+//             answer++;
+//         }
+        
+//         return answer;
         
         Arrays.sort(people);
-        
-        int p1 = 0;
-        int p2 = people.length-1;
-        
-        while(p2 >= p1){
-            int w = people[p2];
-            p2--;
-            if(w + people[p1] <= limit){
-                p1++;
-            }
-            answer++;
+        int i = 0;
+        int j = people.length - 1;
+        for (; i < j; --j) {
+            if (people[i] + people[j] <= limit)
+                ++i;
         }
         
-        return answer;
+        // 한 명씩 태우는 경우의 수 - 두 명씩 태우는 경우의 수
+        return people.length - i; 
 
     }
 }

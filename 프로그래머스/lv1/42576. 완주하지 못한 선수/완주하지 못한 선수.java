53c7c28f668eeb169dc658934c68722dc9264bf8
Leejinju <118646964+jinju1123@users.noreply.github.com>
2023-08-17 23:11:57 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 291.28 ms, Memory: 96.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,20 +3,32 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
        
-        LinkedList<String> list = new LinkedList<>(Arrays.asList(participant)); 
+//         LinkedList<String> list = new LinkedList<>(Arrays.asList(participant)); 
         
-        Collections.sort(list);
+//         Collections.sort(list);
+//         Arrays.sort(completion);
+        
+//         for(int i=0; i<completion.length; i++){
+//             if(list.get(0).equals(completion[i])){
+//                 list.remove(0);
+//             } else if(list.get(1).equals(completion[i])) {
+//                 list.remove(1);
+//             }
+//         }
+        
+//         return list.get(0);
+        
+        // 깔끔한 코드
+        Arrays.sort(participant);
         Arrays.sort(completion);
         
-        for(int i=0; i<completion.length; i++){
-            if(list.get(0).equals(completion[i])){
-                list.remove(0);
-            } else if(list.get(1).equals(completion[i])) {
-                list.remove(1);
+        int i;
+        for ( i=0; i<completion.length; i++){
+            if (!participant[i].equals(completion[i])){
+                return participant[i];
             }
         }
         
-        
-        return list.get(0);
+        return participant[i];
     }
 }

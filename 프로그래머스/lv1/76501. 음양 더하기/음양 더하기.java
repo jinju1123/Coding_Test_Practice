1818c226f30275c95357be11c5432bbbbbc91a60
Leejinju <118646964+jinju1123@users.noreply.github.com>
2023-08-22 13:11:03 +0900

    [level 1] Title: 음양 더하기, Time: 0.04 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -1,28 +1,12 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
-//         int answer = 0;
-        
-//         for(int i=0; i<signs.length; i++){
-//             if(!signs[i]){
-//                 absolutes[i] *= -1;
-//             }
-//         }
-        
-//         for(int i : absolutes) {
-//             answer += i;
-//         }
-        
-//         return answer;
-        
-        // 이렇게 풀면 더 간단!
-        
         int answer = 0;
         
-        for(int i=0; i<signs.length; i++){
-            answer += absolutes[i] * (signs[i] ? 1 : -1);
+        for(int i=0; i<absolutes.length; i++) {
+            absolutes[i] = absolutes[i] * (signs[i] ? 1 : -1);
+            answer += absolutes[i];
         }
         
         return answer;
-        
     }
 }

80ad62ee4228cdf5e635b73b34e791be7c8bb13a
Leejinju <118646964+jinju1123@users.noreply.github.com>
2023-08-22 12:30:58 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,32 +1,16 @@
 class Solution {
     public long solution(long n) {
-//         long answer = -1;
-//         long x = 0;
-//         long i = 1;
         
-//         while(i*i<=n){
-//             if(i*i==n) {
-//                 answer = 1;
-//                 x = i;
-//                 break;
-//             } 
-//             i++;
-//         }
+        long answer = 0;
         
-//         if(answer == 1) {
-//             answer = (x+1)*(x+1);
-//         }
+        long sn = (long)Math.sqrt(n);
         
-//         return answer;
-        
-        // ㅎㅎ pow와 sqrt는 생각했지만 이런 방법은 생각 못했다..
-        // sqrt는 double로 나와서 int를 해준듯
-        
-        if (Math.pow((int)Math.sqrt(n), 2) == n) {
-            return (long) Math.pow(Math.sqrt(n) + 1, 2);
+        if(sn*sn == n) {
+            answer = (sn + 1) * (sn + 1);
+        } else {
+            answer = -1;
         }
-
-        return -1;
         
+        return answer;
     }
 }

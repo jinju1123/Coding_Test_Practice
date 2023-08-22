9f2fff48c83918eafdb1bf4addac32d1acb7e37f
Leejinju <118646964+jinju1123@users.noreply.github.com>
2023-08-22 12:38:38 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.51 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -3,13 +3,13 @@ import java.util.*;
 class Solution {
     public long solution(long n) {
         
-        String str = ""+n;    
+        String str = String.valueOf(n);
         String[] strArr = str.split("");
         
-        // 문자열로 된 숫자("1" 이런거)도 정렬하면 숫자 순대로 정렬된다. 이걸 몰랐다!!
         Arrays.sort(strArr, Collections.reverseOrder());
+        
         str = String.join("", strArr);
-
+        
         return Long.parseLong(str);
     }
 }

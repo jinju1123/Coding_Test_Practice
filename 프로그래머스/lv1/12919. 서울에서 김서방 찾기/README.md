9644c459c92f5632f727e9abedf98c77899d3c6a
Leejinju <118646964+jinju1123@users.noreply.github.com>
2023-08-22 13:00:23 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 5.73 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/README.md
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 서울에서 김서방 찾기 - 12919 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12919?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12919) 
 
 ### 성능 요약
 
-메모리: 76.3 MB, 시간: 9.04 ms
+메모리: 79.3 MB, 시간: 5.73 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,12 +1,11 @@
 class Solution {
     public String solution(String[] seoul) {
-        
+
         int idx = 0;
         
         for(int i=0; i<seoul.length; i++){
-            if(seoul[i].equals("Kim")){
+            if(seoul[i].equals("Kim")) {
                 idx = i;
-                break;
             }
         }
         

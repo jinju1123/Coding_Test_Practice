f8107a165817e99fbb5f499093e594bffaa347d8
Leejinju <118646964+jinju1123@users.noreply.github.com>
2023-08-22 12:14:45 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.04 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.java
+++ 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.java
@@ -1,15 +1,5 @@
 class Solution {
     public int solution(String s) {
-        // int answer = 0;
-        // if(s.charAt(0)=='+'){
-        //     s = s.replaceAll("\\+", "");
-        //     answer = Integer.parseInt(s);
-        // } else {
-        //     answer = Integer.parseInt(s);
-        // }
-        // return answer;
-        
-        // ??????????? 자바가 문자+를 그냥 숫자+로 인식하는지 몰랐다 
-        return  Integer.parseInt(s);
+        return Integer.parseInt(s);
     }
 }

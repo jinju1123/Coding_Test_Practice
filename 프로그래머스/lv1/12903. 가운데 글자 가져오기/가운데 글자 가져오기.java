cd3765715e3156a777eb528a0a4e1b29c14ae8b0
Leejinju <118646964+jinju1123@users.noreply.github.com>
2023-08-26 22:33:54 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -1,14 +1,5 @@
 class Solution {
     public String solution(String s) {
-        // String answer = "";
-        // if(s.length()%2!=0){
-        //     answer = ""+s.charAt(s.length()/2);
-        // } else {
-        //     answer = ""+s.charAt(s.length()/2-1)+s.charAt(s.length()/2);
-        // }
-        // return answer;
-        
-        // 속도에 차이가 있나?
-        return s.substring((s.length()-1)/2, s.length()/2 + 1);
+        return s.substring((s.length()-1)/2, s.length()/2+1);
     }
 }

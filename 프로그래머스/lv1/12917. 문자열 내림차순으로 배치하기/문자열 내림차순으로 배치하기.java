be78e99263715381e5053a11e79f7fb1cc084888
Leejinju <118646964+jinju1123@users.noreply.github.com>
2023-09-08 23:39:38 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.37 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
@@ -8,12 +8,23 @@ class Solution {
         
         // StringBuilder와 친해지기
         
-        char[] charArr = s.toCharArray();
-        Arrays.sort(charArr);
+//        char[] charArr = s.toCharArray();
+//        Arrays.sort(charArr);
         
         // String.valueOf는 매개변수 값을 String 생성자 함수에 전달 후 새로운 문자열 객체를 반환함
         // String.valueOf는 char[]을 String으로 반환해준다. 배열 중에선 문자 배열만 가능한듯
-        StringBuilder sb = new StringBuilder(String.valueOf(charArr));
+//        StringBuilder sb = new StringBuilder(String.valueOf(charArr));
+//        sb.reverse();
+        
+//        return sb.toString();
+        
+        
+        char[] sArr = s.toCharArray();
+        
+        Arrays.sort(sArr);
+        
+        StringBuilder sb = new StringBuilder(String.valueOf(sArr));
+        
         sb.reverse();
         
         return sb.toString();

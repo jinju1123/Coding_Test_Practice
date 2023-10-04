91c5e136eccff0ab5bb89f4271f18827cc979a0d
Leejinju <118646964+jinju1123@users.noreply.github.com>
2023-10-04 23:37:43 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 2.03 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,50 +1,14 @@
 class Solution {
     public String solution(String s) {
-        
-//         String answer = "";
-        
-//         String start = s.stripLeading();
-//         int startnum = s.length() - start.length();
-        
-//         String end = s.stripTrailing();
-//         int endnum = s.length() - end.length();
-        
-//         String[] strArr = s.trim().split(" ");
-        
-//         for(int i=0; i<strArr.length; i++){
-//             for(int j=0; j<strArr[i].length(); j++){
-//                 if(j%2==0) {
-//                     answer += String.valueOf(strArr[i].charAt(j)).toUpperCase();
-//                 } else {
-//                     answer += String.valueOf(strArr[i].charAt(j)).toLowerCase();
-//                 }
-//             }
-//             if(i!=strArr.length-1){
-//                 answer += " ";
-//             }
-//         }
-        
-//         for(int i=0; i<startnum; i++){
-//             answer = " " + answer;
-//         }
-        
-//         for(int i=0; i<endnum; i++){
-//             answer += " ";
-//         }
-        
-//         return answer;
-        
-        // 간단 깔끔... 이거 생각해놓고 이렇게 못 품 ㅠㅠ 언제쯤..?
-        
         String answer = "";
         int cnt = 0;
-        String[] array = s.split("");
-
-        for(String ss : array) {
-            cnt = ss.contains(" ") ? 0 : cnt + 1;
-            answer += cnt % 2 == 0 ? ss.toLowerCase() : ss.toUpperCase(); 
-        }
-        return answer;
+        String[] strArr = s.split("");
         
+        for(String str : strArr) {
+            cnt = str.contains(" ") ? 0 : cnt+1;
+            answer += (cnt % 2 == 0) ? str.toLowerCase() : str.toUpperCase();
+        }
+        
+        return answer;
     }
 }
